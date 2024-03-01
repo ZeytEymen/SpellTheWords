ea084fd58ae28145d3a42ccc47496e3a49781e51
Zeyt Eymen <48064412+ZeytEymen@users.noreply.github.com>
2024-03-02 01:42:13 +0300

    Update Program.cs
    Rename methods

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     public class Program
     {
         static bool isVowelLetter(char ch) => "aeıiuüoöAEIİOÖUÜ".Contains(ch) ? true : false;
-        static bool isEndOfWord(string str) => !str.Any(c => "aeıiuüoöAEIİOÖUÜ".Contains(c));
+        static bool isFirstSpell(string str) => !str.Any(c => "aeıiuüoöAEIİOÖUÜ".Contains(c));
         static void Main(string[] args)
         {
             string word = "Schwarzenegger";//The word you wanna spell it.
@@ -11,7 +11,7 @@
             for (int i = word.Length - 1; i >= 0; i--)
             {
                 spell += word[i];
-                if (isVowelLetter(word[i])  && isEndOfWord(word.Substring(0, i)))
+                if (isVowelLetter(word[i])  && isFirstSpell(word.Substring(0, i)))
                     continue;
                 if (isVowelLetter(word[i]) && !isVowelLetter(word[((i - 1) >= 0) ? (i - 1) : (i)]))
                     spell += word[--i] + "-";
